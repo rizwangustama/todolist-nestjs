eb99fd2c60700965a7f15fa22ff286cb5b7bb273
Rizwan Gustama <rizonegustama@gmail.com>
2024-11-10 02:29:40 +0700

    cors

--- src/main.ts
+++ src/main.ts
@@ -3,6 +3,7 @@ import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  // CORS
   await app.enableCors({
     origin: ['http://localhost:3001', 'http://localhost:3000'],
     methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
